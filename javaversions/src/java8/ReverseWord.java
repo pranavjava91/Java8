cbdc6f567255281f948a68f0030cf387d289e618
krishna <krishna@LAPTOP-VP3KSPPN>
2026-03-07 13:11:04 +0530

    reverse word java8

---
+++ javaversions/src/java8/ReverseWord.java
@@ -0,0 +1,17 @@
+package java8;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ReverseWord {
+
+	public static void main(String[] args) {
+		String str = "Java Concept Of The Day";
+		String result = Arrays.stream(str.split(" ")).map(s -> new StringBuilder(s).reverse()).collect(Collectors.joining(" "));
+		System.out.println(result);
+	}
+	
+	// joining converts individual elements to a string
+
+}
